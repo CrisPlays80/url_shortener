030d0729ff4f2ebd3164b96afae2d450a3265e8d
CrisPlays80 <cristian.steven05@gmail.com>
2025-06-23 14:02:21 -0500

    cambios en formatos

--- urlsite/asgi.py
+++ urlsite/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'urlsite.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "urlsite.settings")
 
 application = get_asgi_application()
--- urlsite/wsgi.py
+++ urlsite/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'urlsite.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "urlsite.settings")
 
 application = get_wsgi_application()
